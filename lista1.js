ea8b1dd7e41ade7c3b2647abe65b0ef385ce355b
kaykay <clara.moreira.rodrigues@escola.pr.gov.br>
2026-02-26 14:29:27 +0000

    resolução da lista

---
+++ lista1.js
@@ -0,0 +1,33 @@
+//Criar um programa para exibir a mensagem: "Olá, mundo."
+
+let mensagem= 'Olá, mundo';
+console.log(mensagem);
+
+//Criar um programa para exibir seu nome completo
+
+let nome= 'Kayque'
+console.log(nome);
+
+//Criar um programa que mostre 1 numero e exiba seu dobro
+
+let numero= 40;
+let dobro= numero * 2;
+console.log(dobro)
+
+//Criar um programa que mostre em numero e exiba seu triplo
+
+let number= 30;
+let triplo= number * 3;
+console.log(triplo);
+
+//Criar um programa que mostre duas notas e calcule a media
+
+let nota1= 20;
+let nota2= 60;
+let media= nota1 + nota2 /2
+
+//Criar um programa para converter metros em centimetros
+//Criar um programa para mostrar o antecessor e o sucessor de um numero
+//Criar um programa que mostre a area de um retangulo
+//Criar um programa para calcular um aumento de 10% num salario de 2000,00
+//Criar um programa para verificar se um numero é par ou impar
