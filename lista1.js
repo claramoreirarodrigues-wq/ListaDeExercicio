eb36d2ead823e966f63136f98cd08a6fbbc3aa06
kaykay <clara.moreira.rodrigues@escola.pr.gov.br>
2026-02-27 12:35:42 +0000

    lista2

--- lista1.js
+++ lista1.js
@@ -6,28 +6,48 @@ console.log(mensagem);
 //Criar um programa para exibir seu nome completo
 
 let nome= 'Kayque'
-console.log(nome);
+console.log(`Meu nome é ${nome}`);
 
 //Criar um programa que mostre 1 numero e exiba seu dobro
 
 let numero= 40;
 let dobro= numero * 2;
-console.log(dobro)
+console.log(`O dobro de ${numero} é ${dobro}`);
 
 //Criar um programa que mostre em numero e exiba seu triplo
 
-let number= 30;
-let triplo= number * 3;
-console.log(triplo);
+let num77= 15;
+let triplo= num77 * 3;
+console.log(`O triplo de ${num77} é ${triplo}`);
 
 //Criar um programa que mostre duas notas e calcule a media
 
-let nota1= 20;
+let nota1= 30;
 let nota2= 60;
-let media= nota1 + nota2 /2
+let media= (nota1 + nota2) /2;
+console.log(`A média é ${media}`);
 
 //Criar um programa para converter metros em centimetros
+
+let metros= 15;
+let centimetros= metros * 100;
+console.log(`${metros} metros são ${centimetros}`);
+
 //Criar um programa para mostrar o antecessor e o sucessor de um numero
+
+let numero5= 5;
+let sucessor= numero5 + 1;
+let antecessor= numero5 - 1;
+console.log(`O antecessor de ${numero5} é ${antecessor}, e o sucessor é ${sucessor}`);
+
 //Criar um programa que mostre a area de um retangulo
+
 //Criar um programa para calcular um aumento de 10% num salario de 2000,00
 //Criar um programa para verificar se um numero é par ou impar
+
+let numero8= 77;
+if(numero8 %2 === 0) {
+    console.log('Numero par');
+} else {
+    console.log('Numero impar');
+}
---
+++ lista2.js
@@ -0,0 +1,26 @@
+//Criar um programa que mostre um programa que mostre se o numero é positivo ou negativo
+
+let numero= -5;
+if (numero >= 0) {
+    console.log('Positivo')
+} else {
+    console.log('Negativo')
+}
+
+//Criar  um programa que mostre se um ano é bisexto
+
+let ano= 2000;
+if (ano %4 === 0) {
+    console.log('É um ano bisexto')
+} else {
+    console.log('Não é um ano bisexto')
+}
+
+//Criar um programa que mostre uma senha e a mensagem de acesso rapido
+
+let senha= "ablubluble14";
+if(senha === "ablubluble14") {
+    console.log("Acesso pertitido")
+} else {
+    console.log("Acesso negado")
+}
