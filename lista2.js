cb7b71cf812f4345ebc2680f5585a096c014afb5
kaykay <clara.moreira.rodrigues@escola.pr.gov.br>
2026-03-05 14:11:10 +0000

    lista2

--- lista2.js
+++ lista2.js
@@ -29,3 +29,39 @@ if(senha === "ablubluble14") {
 //Criar um programa que classifique uma pessoa como criança, adolecente ou adulto de acordo com a idade
 //Criar um programa que mostre qual tipo de triangulo de acordo com seus lados (equilatero, isoceles, e escalneo)
 //Criar um programa que mostre os conceitos A, B, C ou D de acordo com as notas: >9.5 A; entre 9.4 e 8.5 B; entre 8.4 e 7.0 C; <6.9 D
+//Criar um programa para verificar se o numero 12 é divisível por 2 e por 3
+
+let num = 12;
+if (num % 2 === 0 && num % 3 === 0) {
+    console.log('Divisivel por 2 e por 3')
+} else {
+    console.log('Não é divisivel por 2 e por 3')
+}
+
+//Criar um programa para mostrar um desconto de 12% paea produtos acima de R$80
+
+let preco = 700;
+let desconto = preco * 0.88
+if (preco >= 800){
+    console.log(`O valor do desconto é ${desconto}`)
+} else {
+    console.log('Não tem desconto')
+}
+
+//Criar um programa que mostre os numeros de 1 a 10 usando laço de repetição
+
+for(let i=1;i<=10;i++){
+    console.log(i)
+}
+
+//Criar um programa para mostrar apenas os numeros pares de 1 a 20
+for(let i=10; i>=1; i--){
+    console.log(i)
+}
+//Criar um programa para mostrar apenas os numeros impares de 1 a 20
+//Criar um programa para mostrar a taboada do 5
+//Criar um programa para mostrar os numeros de 1 a 7 usando while
+//Criar um programa para mostrar a taboada do 3 usando while
+//Criar um programa para mostrar o fatorial de 5
+//Criar um programa para saber se o numero 7 é primo
+//Criar um programa para calcular a potencia de 2 elevado a 3
